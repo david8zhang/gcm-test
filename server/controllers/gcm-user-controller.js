bb017cd51422edf1d2df4a64f950562768fa2a01
David Zhang <david8zhang@berkeley.edu>
2016-01-18 22:39:35 -0800

    added some stuff

--- server/controllers/gcm-user-controller.js
+++ server/controllers/gcm-user-controller.js
@@ -2,9 +2,45 @@ var AWS = require('aws-sdk');
 var sha1 = require('sha-1');
 var docClient = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' });
 
-/** Get the user based on the id. */
+/** GET the user based on the id. */
 exports.getUser = function(req, res) {
 	var user_id = req.query.user_id;
+	params = {};
 	params.TableName = 'GCM-users';
+	params.FilterExpression = "user_id = :user_id";
+	params.ExpressionAttributeValues = {
+		":user_id" : user_id
+	};
+	docClient.scan(params, function(err, data) {
+		if(err) {
+			res.send(err);
+		} else {
+			res.json(data);
+		}
+	})
+}
 
+/** POST a new user to the database. */
+exports.createUser = function(req, res) {
+	var reg_token = req.body.reg_token;
+	var user_id = sha1(Math.floor(Date.now() / 1000).toString());
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var params = {};
+	params.TableName = 'GCM-users'
+	params.Item = {
+		user_id: user_id,
+		email: email,
+		username: username,
+		password: password,
+		reg_token: reg_token
+	}
+	docClient.put(params, function(err, data) {
+		if(err) {
+			res.send(err);
+		} else {
+			res.status(200).send("Success! User was created!");
+		}
+	})
 }
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var gcmController = require('../controllers/gcm-obj-controller.js');
+var gcmObjController = require('../controllers/gcm-obj-controller.js');
+var gcmUserController = require('../controllers/gcm-user-controller.js');
 
 //API Endpoints
 router.route('/gcm/index')
-	.get(gcmController.getObjs);
+	.get(gcmObjController.getObjs);
 
 router.route('/gcm/upload')
-	.post(gcmController.postObj);
+	.post(gcmObjController.postObj);
+
+router.route('/gcm/users')
+	.get(gcmUserController.getUser);
+
+router.route('/gcm/users/register')
+	.post(gcmUserController.createUser);
 
 module.exports = router;
